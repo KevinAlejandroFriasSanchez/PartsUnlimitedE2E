a7c180e22096b4767bed5acf24df972af253b3e5
Kevin Alejandro Frias Sanchez <alejandro.atem@gmail.com>
2023-04-05 07:44:16 -0600

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //my firts change
+        //My second change
         [Key]
         public int CartItemId { get; set; }
 
